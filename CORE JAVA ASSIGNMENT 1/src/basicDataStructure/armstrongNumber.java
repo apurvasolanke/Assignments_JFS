e027c83f3a1236b527a03a9fff0156ba95c08ed2
apurva solanke <APURVA.RAOSAHEB-SOLANKE@CAPGEMINI.COM>
2022-06-18 10:45:23 +0530

    assignment uploaded

---
+++ CORE JAVA ASSIGNMENT 1/src/basicDataStructure/armstrongNumber.java
@@ -0,0 +1,50 @@
+package basicDataStructure;
+
+import java.util.Scanner;
+
+public class armstrongNumber {
+	static boolean isArmstrong(int n)
+	{
+		int temp,digit =0,last=0,sum=0;
+		
+		temp =n;
+		
+		while(temp>0)
+		{
+			temp=temp/10;
+			digit++;
+		}
+		temp=n;
+		while(temp>0)
+		{
+			last = temp% 10;
+			
+			sum += (Math.pow(last, digit));	
+			
+			temp=temp/10;
+		}
+		if(n==sum)
+			
+			return true;
+		else
+			 return false;
+	}
+	
+	public static void main(String[] args) {
+		int num;
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter the number");
+		
+		num=sc.nextInt();
+		if(isArmstrong(num))
+		{
+			System.out.println("Armstrong");
+			
+		}
+		else
+		{
+			System.out.println("Not Armstrong");
+		}
+	}
+
+}
